ebe5242f42a19d15b7de6ebe5824b5f1b9282e0b
Bernhard Fritz <bernhard.e.fritz@gmail.com>
2023-11-26 16:02:33 +0100

    add solution for problem 1

---
+++ 1-two-sum/main.ts
@@ -0,0 +1,36 @@
+import { assertEquals } from "https://deno.land/std@0.207.0/assert/mod.ts";
+
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  throw new Error("No solution!");
+}
+
+Deno.test("Example 1", () => {
+  const input: [nums: number[], target: number] = [[2, 7, 11, 15], 9];
+
+  const output = twoSum(...input);
+
+  assertEquals(output, [0, 1]);
+});
+
+Deno.test("Example 2", () => {
+  const input: [nums: number[], target: number] = [[3, 2, 4], 6];
+
+  const output = twoSum(...input);
+
+  assertEquals(output, [1, 2]);
+});
+
+Deno.test("Example 3", () => {
+  const input: [nums: number[], target: number] = [[3, 3], 6];
+
+  const output = twoSum(...input);
+
+  assertEquals(output, [0, 1]);
+});
